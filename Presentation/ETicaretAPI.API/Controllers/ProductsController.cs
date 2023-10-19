2c5904a4492d50738fcc3ac3fb846636b7b81b36
Atakan Arslan <atak.arslan@hotmail.com>
2023-10-19 21:16:53 +0300

    re

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,7 +19,7 @@ namespace ETicaretAPI.API.Controllers
             _productReadRepository = productReadRepository;
         }
         [HttpGet]
-        public async void Get()
+        public async Task Get()
         {
              await _productWriteRepository.AddRangeAsync(new()
             {
@@ -27,6 +28,13 @@ namespace ETicaretAPI.API.Controllers
                 new() {Id = Guid.NewGuid(), Name ="Product 3", Price = 200, CreatedDate = DateTime.UtcNow, Stock = 30},
             });
             var count = await _productWriteRepository.SaveAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            Product product = await _productReadRepository.GetByIdAsync(id);
+            return Ok(product);
         }
     }
 }
